ce86f1b7d7a1450dda090470bf297d321eb84104
samarthupadhyay90 <samarthupadhyay68@gmail.com>
2024-02-16 17:12:15 +0530

    all files added

---
+++ style.js
@@ -0,0 +1,17 @@
+// const database = [
+//     { title: "Example Title 1", content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.", link: "oxygen bpc flowmeter.html" },
+//     { title: "Example Title 2", content: "Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.", link: "hospital iv hanger.html" },
+//     { title: "Example Title 3", content: "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.", link: "hospital curtain.html" }
+//   ];
+
+//   function search() {
+//     const searchTerm = document.getElementById("searchInput").value.toLowerCase();
+//     const searchResult = database.find(item => item.title.toLowerCase().includes(searchTerm) || item.content.toLowerCase().includes(searchTerm));
+//     if (searchResult) {
+//       // Redirect to the inner page associated with the search result
+//       window.location.href = searchResult.link;
+//     } else {
+//       // Handle case where no result is found
+//       alert("No results found");
+//     }
+//   }
